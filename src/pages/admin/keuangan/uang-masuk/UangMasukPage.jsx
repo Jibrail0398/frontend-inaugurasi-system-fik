6eb249a0adfaf3949c67d90833a04e175bcc3822
Taufik Hidayat <opik.1320@gmail.com>
2025-09-22 19:46:46 +0700

    perbaikan UangMasukPage

--- src/pages/admin/keuangan/uang-masuk/UangMasukPage.jsx
+++ src/pages/admin/keuangan/uang-masuk/UangMasukPage.jsx
@@ -106,7 +106,7 @@ const UangMasukPage = () => {
     return (
         <>
             <div className="d-sm-flex align-items-center justify-content-between mb-4">
-                <h1 className="h3 mb-0 text-gray-800">Event</h1>
+                <h1 className="h3 mb-0 text-gray-800">Uang Masuk</h1>
             </div>
             <div className="row">
                 {/* Total Uang Masuk */}
