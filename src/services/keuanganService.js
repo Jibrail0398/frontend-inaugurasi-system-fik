bc12dfadfab0d74f899ceedd0d0ce526b5e69b7b
Taufik Hidayat <opik.1320@gmail.com>
2025-10-18 11:56:03 +0700

    Perbaikan keuangan service

--- src/services/keuanganService.js
+++ src/services/keuanganService.js
@@ -4,12 +4,41 @@
  */
 
 /**
+ * Content-Type: multipart/form-data
  * @typedef CreateUangMasukRequest
  * @property {number} jumlah_uang_masuk
  * @property {string} asal_pemasukan
- * @property {string} bukti_pemasukan
+ * @property {*} bukti_pemasukan
  * @property {number} keuangan_id
  * @property {string} tanggal_pemasukan
+ */
+
+/**
+ * Content-Type: multipart/form-data
+ * @typedef CreateUangKeluarRequest
+ * @property {number} jumlah_pengeluaran
+ * @property {string} asal_pengeluaran
+ * @property {*} bukti_pengeluaran
+ * @property {number} keuangan_id
+ * @property {string} tanggal_pengeluaran
+ */
+
+/**
+ * Update Uang Masuk
+ * @typedef UpdateUangMasukRequest
+ * @property {number} jumlah_uang_masuk
+ * @property {string} asal_pemasukan
+ * @property {string} tanggal_pemasukan
+ * @property {number} keuangan_id
+ */
+
+/**
+ * Update Uang Keluar
+ * @typedef UpdateUangKeluarRequest
+ * @property {number} jumlah_pengeluaran
+ * @property {string} asal_pengeluaran
+ * @property {string} tanggal_pengeluaran
+ * @property {number} keuangan_id
  */
 
 import axios from "axios";
@@ -47,9 +76,10 @@ export const getAllUangMasuk = async (token) => {
 /**
  * Get all uang keluar
  * @param {string} token
+ * @returns {Promise<Object>}
  */
-export const getAllUangKeluar = (token) => {
-    const response = axios.get(`${BASE_URL_API}/uang-keluar`, {
+export const getAllUangKeluar = async (token) => {
+    const response = await axios.get(`${BASE_URL_API}/uang-keluar/index`, {
         headers: {
             Authorization: `Bearer ${token}`,
         },
@@ -61,6 +91,7 @@ export const getAllUangKeluar = (token) => {
  * Create uang masuk
  * @param {CreateUangMasukRequest} data
  * @param {string} token
+ * @returns {Promise<Object | never>}
  */
 export const createUangMasuk = async (data, token) => {
     const response = await axios.post(`${BASE_URL_API}/uang-masuk/add`, data, {
@@ -72,10 +103,29 @@ export const createUangMasuk = async (data, token) => {
     return response.data;
 };
 
-export const createUangKeluar = (data, token) => {
-    //
+/**
+ * Create uang keluar
+ * @param {CreateUangKeluarRequest} data
+ * @param {string} token
+ * @returns {Promise<Object | never>}
+ */
+export const createUangKeluar = async (data, token) => {
+    const response = await axios.post(`${BASE_URL_API}/uang-keluar/add`, data, {
+        headers: {
+            Authorization: `Bearer ${token}`,
+            "Content-Type": "multipart/form-data",
+        },
+    });
+    return response.data;
 };
 
+/**
+ * Update uang masuk
+ * @param {number} id ID uang masuk
+ * @param {UpdateUangMasukRequest} data Request data uang masuk
+ * @param {string} token Auth token
+ * @returns {Promise<Object | never>}
+ */
 export const updateUangMasuk = async (id, data, token) => {
     const response = await axios.put(`${BASE_URL_API}/uang-masuk/update/${id}`, data, {
         headers: {
@@ -85,10 +135,28 @@ export const updateUangMasuk = async (id, data, token) => {
     return response.data;
 };
 
-export const updateUangKeluar = (id, { data }, token) => {
-    //
+/**
+ *
+ * @param {number} id ID Uang Keluar
+ * @param {UpdateUangKeluarRequest} data
+ * @param {string} token Auth token
+ * @returns {Promise<Object | never>}
+ */
+export const updateUangKeluar = async (id, data, token) => {
+    const response = await axios.put(`${BASE_URL_API}/uang-keluar/update/${id}`, data, {
+        headers: {
+            Authorization: `Bearer ${token}`,
+        },
+    });
+    return response.data;
 };
 
+/**
+ * Hapus Uang Masuk
+ * @param {number} id ID Uang Masuk
+ * @param {string} token Auth token
+ * @returns {Promise<Object | never>}
+ */
 export const deleteUangMasuk = (id, token) => {
     const response = axios.delete(`${BASE_URL_API}/uang-masuk/delete/${id}`, {
         headers: {
@@ -98,6 +166,16 @@ export const deleteUangMasuk = (id, token) => {
     return response.data;
 };
 
-export const deleteUangKeluar = (id, token) => {
-    //
+/**
+ * Hapus Uang Keluar
+ * @param {number} id ID Uang Keluar
+ * @param {string} token Auth token
+ */
+export const deleteUangKeluar = async (id, token) => {
+    const response = await axios.delete(`${BASE_URL_API}/uang-keluar/delete/${id}`, {
+        headers: {
+            Authorization: `Bearer ${token}`,
+        },
+    });
+    return response.data;
 };
